f7d81fa6da987c1c7e1e31d5ba195a02fb851f79
sharafath ali <sharafathaliofficial@gmail.com>
2022-10-20 22:51:02 +0530

    second

---
+++ datatype.py
@@ -0,0 +1,25 @@
+from platform import java_ver
+from typing import Type
+
+#list
+a=[1,'helo',3]
+print(a[1])
+print(type(a))
+#tuple
+a=(1,'helo',5)
+print(a[0])
+print(type(a))
+#string
+name="jima"
+print(name)
+print(type(name))
+#dictionary
+ed={'a':1,'b':22}
+print(ed['b'])
+print(type(ed))
+#int
+s=1
+print(type(s))
+#ffloat
+u=9.6
+print(type(u))
